7bf236d976a820ee0f9163401e5b3ad46197dcc5
Lace Lofranco <lace.lofranco@microsoft.com>
2018-08-25 22:47:56 +1000

    aks deployment

---
+++ deploy-aks/README.md
@@ -0,0 +1,31 @@
+# Deploy MLFlow Tracking Server on Azure Kubernetes Service (AKS)
+
+## Deploy
+
+### Requirements: 
+- [az cli 2.0+](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
+- [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+
+### Deployment
+
+1. Ensure you are in the `deploy-aks` folder.
+2. Open `deploy.sh` and `mlflowtracking.yaml` inspect/change top parameters and environment variables, if necessary.
+3. Run `./deploy.sh`.
+4. Validate deployment by navigating to the deployed Kubernetes Service IP:port (default: 5000).
+   - You can retrieve IP and port by running: 
+   - `kubectl get services`
+   - NOTE: First time navigating to the web-ui may throw an error initially but I've found this to be transient.
+## Architecture
+The following shows the architecture of the deployment.
+
+![AKS Architecture](../images/aks-archi.PNG?raw=true "ACI Architecture")
+
+## Connecting to MLFlow Tracking Server
+The following outlines requirements to connect to the MLFlow Tracking Server.
+
+1. Set `MLFLOW_TRACKING` environment variable to the IP:port (default: 5000) of the deployed Tracking Server.
+2. Since this deployment uses Azure Blob Storage as the Artifact Store, MLFlow requires either `AZURE_STORAGE_CONNECTION_STRING` or `AZURE_STORAGE_ACCESS_KEY` environment variables set appropriately at server **and at the client** as well.
+3. Ensure you have `azure-storage` python installed.
+   - Run `pip install azure-storage` to install the package.
+  
+More information here: https://www.mlflow.org/docs/latest/tracking.html#logging-data-to-runs
---
+++ deploy-aks/deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace # For debugging
+
+#####################
+# CONFIGURE PARAMS
+
+RG_NAME=mlflowserver-aks-rg-03
+RG_LOCATION=australiaeast
+
+AKS_IMAGE=devlace/mlflowserver-azure:latest
+AKS_NAME=mlflowaks
+AKS_STORAGE_ACCOUNT_NAME=storage$RANDOM
+AKS_STORAGE_CONTAINER_NAME=mlflow
+
+#################
+# DEPLOY
+
+echo "Creating resource group: $RG_NAME"
+az group create --name "$RG_NAME" --location "$RG_LOCATION"
+
+echo "Creating AKS cluster: $AKS_NAME"
+az aks create \
+    --resource-group $RG_NAME \
+    --name $AKS_NAME \
+    --node-count 1 \
+    --enable-addons monitoring \
+    --generate-ssh-keys
+
+echo "Retrieving credentials of AKS cluster: $AKS_NAME"
+az aks get-credentials \
+    --resource-group $RG_NAME \
+    --name $AKS_NAME
+
+echo "Creating storage account: $AKS_STORAGE_ACCOUNT_NAME"
+az storage account create \
+    --resource-group $RG_NAME \
+    --location $RG_LOCATION \
+    --name $AKS_STORAGE_ACCOUNT_NAME \
+    --sku Standard_LRS
+
+# Mlflow requires environment variable (AZURE_STORAGE_ACCESS_KEY) to be set at client and with Server
+# Export the access keyas an environment variable
+echo "Exporting storage keys: $AKS_STORAGE_ACCOUNT_NAME"
+export AZURE_STORAGE_ACCESS_KEY=$(az storage account keys list --resource-group $RG_NAME --account-name $AKS_STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
+
+# Build blob storage fqdn of ml artifacts
+AKS_STORAGE_FQDN_ARTIFACT="wasbs://$AKS_STORAGE_CONTAINER_NAME@$AKS_STORAGE_ACCOUNT_NAME.blob.core.windows.net/mlartefacts"
+
+echo "Create kubernetes secret"
+kubectl create secret generic storage-secret \
+    --from-literal=azurestorageaccountartifact="$AKS_STORAGE_FQDN_ARTIFACT" \
+    --from-literal=azurestorageaccountkey="$AZURE_STORAGE_ACCESS_KEY"
+
+echo "Deploying MLFlow tracking server to AKS"
+kubectl apply -f mlflowtracking.yaml
